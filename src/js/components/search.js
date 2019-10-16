83448ff9812cbe69fb3e9d9da079e8fa2427d727
Foad Yousefi <foadyousefi@gmail.com>
2019-10-16 12:36:56 +0200

    Improve saerch input in the header menu

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,6 +1,7 @@
 class Search {
   constructor() {
     this.searchBtn = document.querySelector('.search-icon')
+    this.searchInput = document.querySelector('.search-input')
   }
 
   events() {
@@ -11,18 +12,26 @@ class Search {
       searchParent.querySelector('.main-search').style.width = menuItem.offsetWidth + 'px';
       searchParent.querySelector('.main-search').style.right = searchParent.offsetWidth - 10 + 'px'
       searchParent.classList.toggle('search-open');
+      setTimeout(() => {
+        this.searchInput.focus()
+      }, 200);
     });
     document.addEventListener('click', e => {
       const outsideElement = document.querySelector(".header-search");
       let targetElement = e.target;
       do {
         if (targetElement == outsideElement) {
-            return;
+          return;
         }
         targetElement = targetElement.parentNode;
       } while (targetElement);
       this.searchBtn.parentElement.classList.remove('search-open');
     });
+    document.addEventListener('keyup', e => {
+      if (document.querySelector('.search-open') != null && e.key == 'Escape') {
+        document.querySelector('.search-open').classList.remove('search-open')
+      }
+    })
   }
 
   init() {
